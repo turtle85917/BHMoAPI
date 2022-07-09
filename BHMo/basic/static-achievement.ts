26ff031626f54784245a949fdd1d2e67c5351c5d
slime_turtle <61219276+turtle85917@users.noreply.github.com>
2022-07-09 12:00:42 +0900

    🎈 add BHMo category

---
+++ BHMo/basic/static-achievement.ts
@@ -0,0 +1,19 @@
+
+// 🎈 기본 > 업적 조회
+
+interface achievementData {
+    id: string;
+    point: number;
+    extends?: string;
+    rarity: number;
+}
+
+export interface staticAchievement {
+    type: string;
+    version: string;
+    data: achievementData;
+    names: {
+        ko: string;
+        en: string;
+    };
+}
